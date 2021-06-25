2af823faab62a2cbb3c144cd895718a069c62260
JMax45 <maxmailru.45@gmail.com>
2021-06-25 10:00:17 +0200

    feat: add RecentlySearched functionality

--- src/Search/RecentlySearched.tsx
+++ src/Search/RecentlySearched.tsx
@@ -1,9 +1,16 @@
 import './RecentlySearched.css';
 
-const RecentlySearched = () => {
+interface Props {
+    items: string[];
+    onClickEvent: (query: string) => void;
+}
+
+const RecentlySearched: React.FC<Props> = ({ items, onClickEvent }) => {
     return (
         <div className="RecentlySearched">
-
+            {items.map(e => (
+                <p onClick={() => onClickEvent(e)}>{e}</p>
+            ))}
         </div>
     )
 }
--- src/Search/SearchBar.tsx
+++ src/Search/SearchBar.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import RecentlySearched from './RecentlySearched';
 import './SearchBar.css';
 
@@ -8,21 +8,45 @@ interface Props {
 
 const SearchBar: React.FC<Props> = ({ onSearch }) => {
   const [query, setQuery] = useState('');
+  const [recentlySearched, setRecentlySearched] = useState<string[]>([]);
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    onSearch(query);
     setQuery('');
+    search(query);
+  }
+  const search = (toSearchQuery: string) => {
+    onSearch(toSearchQuery);
+    const shifted = recentlySearched.length >= 3 ? recentlySearched.slice(0, -1) : recentlySearched;
+    setRecentlySearched([toSearchQuery, ...shifted]);
+    window.localStorage.setItem('raincloud-recently-searched', JSON.stringify([toSearchQuery, ...shifted]));
   }
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setQuery(e.target.value);
   }
+  useEffect(() => {
+    const searched = window.localStorage.getItem('raincloud-recently-searched');
+    if(searched === null) {
+        setRecentlySearched([]);
+        return;
+    }
+    try {
+        const parsed = JSON.parse(searched);
+        if(Array.isArray(parsed)) {
+            setRecentlySearched(parsed);
+        } else {
+            setRecentlySearched([]);
+        }
+    } catch (error) {
+        setRecentlySearched([]);
+    }
+  }, [])
   return(
     <form className="search-bar" onSubmit={onSubmit}>
       <div>
         <input type="text" placeholder="Search.." name="search" value={query} onChange={onChange} />
         <button type="submit"><i className="fas fa-arrow-right"></i></button>
       </div>
-      <RecentlySearched />
+      <RecentlySearched items={recentlySearched} onClickEvent={(query) => search(query)} />
     </form>
   )
 }
